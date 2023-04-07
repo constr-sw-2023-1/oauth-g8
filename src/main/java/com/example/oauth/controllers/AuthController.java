a134cde31adee067a699d9cde75e7ebd9186bc1c
Tiago <tiago.villa@ve3.com.br>
2023-04-07 10:20:51 -0300

    Adiciona entity para o login

--- src/main/java/com/example/oauth/controllers/AuthController.java
+++ src/main/java/com/example/oauth/controllers/AuthController.java
@@ -1,18 +1,25 @@
 package com.example.oauth.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.oauth.Entity.LoginForm;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/login")
 public class AuthController {
 
-    @GetMapping
-    public String index() {
-        return "Login";
+    @PostMapping
+    public ResponseEntity<?> login(@RequestBody LoginForm loginForm) {
+        return new ResponseEntity<>("Opa", HttpStatus.OK);
     }
 
 }
