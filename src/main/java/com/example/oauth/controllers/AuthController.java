24f234e4ee8edc4e9007795eccffe2ddca3ed779
Tiago <tiago.villa@ve3.com.br>
2023-04-07 15:32:57 -0300

    Adiciona chamada para o Keycloak na rota de login

--- src/main/java/com/example/oauth/controllers/AuthController.java
+++ src/main/java/com/example/oauth/controllers/AuthController.java
@@ -1,25 +1,49 @@
 package com.example.oauth.controllers;
 
+import java.net.http.HttpResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.oauth.Entity.LoginForm;
+import com.example.oauth.services.AuthService;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/login")
+@RequestMapping(value = "/login", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
 public class AuthController {
 
-    @PostMapping
-    public ResponseEntity<?> login(@RequestBody LoginForm loginForm) {
-        return new ResponseEntity<>("Opa", HttpStatus.OK);
+    @Autowired
+    private AuthService authService;
+
+    private LoginForm loginForm;
+
+    @PostMapping()
+    public ResponseEntity<?> login(
+            @RequestParam("client_id") String clientId,
+            @RequestParam("client_secret") String clientSecret,
+            @RequestParam("grant_type") String grantType) {
+
+        // loginForm.setClientId(clientId);
+        // loginForm.setClientSecret(clientSecret);
+        // loginForm.setGrantType(grantType);
+
+        try {
+            HttpResponse<?> response = authService.login(clientId, clientSecret, grantType);
+            return new ResponseEntity<>(response.body(), HttpStatus.OK);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            return new ResponseEntity<>("Foi", HttpStatus.BAD_REQUEST);
+        }
+
     }
 
 }
